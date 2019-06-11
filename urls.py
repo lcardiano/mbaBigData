63d81a8703c320a9ba5267ac2424287ddc306b5d
Lincon Cardiano <41244113+lcardiano@users.noreply.github.com>
2019-06-10 21:34:42 -0300

    Update urls.py
    Sem funcionar o grafico.

--- urls.py
+++ urls.py
@@ -8,8 +8,9 @@ sc = SparkContext('local[*]', 'Top Urls')
 input = sc.textFile('/home/lcardiano/code/erro')
 
 print("Lines : %s" % input.count())
-labels = ['Cookies', 'Jellybean', 'Milkshake', 'Cheesecake']
+labels = [1, 2, 3, 4]
 sizes = [38.4, 40.6, 20.7, 10.3]
+colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
 log_regex = '(?P<ip>[(\d\.)]+) - - \[(?P<date>.*?) -(.*?)\] "(?P<method>\w+) (?P<request_path>.*?) HTTP/(?P<http_version>.*?)" (?P<status_code>\d+) (?P<response_size>\d+) "(?P<referrer>.*?)" "(?P<user_agent>.*?)"'
 top_urls = input \
     .map(lambda line: re.match(log_regex, line)) \
@@ -19,14 +20,14 @@ top_urls = input \
     .countByValue()
 
 for url, count in sorted(top_urls.items(), key=itemgetter(1), reverse=True)[:4]:
-    labels = [format(url)]
-    sizes = [format(count)]
-    #print('{0: >2} => {1: >2}'.format(url, count))
+    
+    labels[url,count] = format(url)
+    sizes[url, count] = format(count)
+    patches, texts = plt.pie(sizes[x], colors=colors, shadow=True, startangle=90)
+    plt.legend(patches, labels[x], loc="best")
+    plt.axis('equal')
+    plt.tight_layout()
+    
+    print('{0: >2} => {1: >2}'.format(url, count))
 
-#print(labels)
-colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
-patches, texts = plt.pie(sizes, colors=colors, shadow=True, startangle=90)
-plt.legend(patches, labels, loc="best")
-plt.axis('equal')
-plt.tight_layout()
 plt.show()
